a5612ac130a4aba0d5d151989f5d7c93b8241ca9
Mauricio Castillo <mauriciocastillocastro@hotmail.com>
2022-08-12 12:03:09 -0500

    DRY code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,11 @@
 class ArticlesController < ApplicationController
 
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+    
+    
     def show
         #debugger -> To make a debug and show in the console de details of the params
-        @article = Article.find(params[:id])
+        
     end
 
 
@@ -16,11 +19,11 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
+       
     end
 
     def create
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(article_params)
         if @article.save
             flash[:notice] = "Article was created succesfully."
         redirect_to @article
@@ -33,8 +36,8 @@ class ArticlesController < ApplicationController
     end
 
     def update
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description))
+       
+        if @article.update(article_params)
             flash[:notice] = "Article was updated succesfully."
             redirect_to @article
         else
@@ -43,10 +46,21 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-        @article = Article.find(params[:id])
+        
         @article.destroy
         redirect_to articles_path, status: :see_other
     end
+
     
+    private
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+    
+
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
 
 end
